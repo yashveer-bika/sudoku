c16e5bd9b73cfca1394edf3ae09c3a663e31c75c
Superman37891 <138830287+Superman37891@users.noreply.github.com>
2024-06-20 18:17:29 -0500

    Starter Board / Cell interface and functions
    Starter board / cell interface and functions, such as setting up the board as a 9x9 array of cells and functions to validate the board and cells for legality with respect to the rules of Sudoku, as well as to set the values of cells and track possible values for unsolved cells

---
+++ Sudoku_Solver/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board();
+        System.out.println(board);
+        board.make_board();
+        System.out.println(board);
+    }
+}
