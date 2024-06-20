d0460cd35c44b457e4d50e3764e51d3dcad23817
Superman37891 <138830287+Superman37891@users.noreply.github.com>
2024-06-20 18:14:56 -0500

    Delete Sudoku_Solver directory
    will re-add

--- Sudoku_Solver/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Board board = new Board();
-        System.out.println(board);
-        board.make_board();
-        System.out.println(board);
-    }
-}
