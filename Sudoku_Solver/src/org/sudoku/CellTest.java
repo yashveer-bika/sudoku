85e130fd74f9b29d62eeb064eb0ade8a4431ac0b
Superman37891 <138830287+Superman37891@users.noreply.github.com>
2024-07-02 14:54:47 -0500

    Completed Sudoku Solver for valid puzzles
    Completed Sudoku Solver for legal, 1-solution puzzles including those that involve guessing and checking values to solve!

---
+++ Sudoku_Solver/src/org/sudoku/CellTest.java
@@ -0,0 +1,43 @@
+package org.sudoku;
+
+class CellTest {
+
+    Cell cell1 = new Cell(1, 1, 1);
+
+    @org.junit.jupiter.api.Test
+    void initPossibleValues() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void removePossibleValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPossibleValues() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getIsNull() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNumPossibleValues() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRow() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCol() {
+    }
+}
